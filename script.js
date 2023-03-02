fbbf5d979bf0e6973766b9178bb3eba37974ba0e
preeti1098 <124133826+preeti1098@users.noreply.github.com>
2023-03-03 00:01:56 +0530

    update-data

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log('hello-world')
+
+
+
+let usernumber = prompt('What is your number')
+document.querySelector('.number').innerText = usernumber
+
+
+
+if(usernumber <= 30){
+    document.querySelector('.result').innerText = ' Sorry ! You are Fail.'
+
+
+}else if (usernumber <= 35){
+    document.querySelector('.result').innerText = ' Sorry ! You are Reappear.'
+
+
+}else{
+    document.querySelector('.result').innerText = ' Congratulations You are Pass.'
+}
+
+
